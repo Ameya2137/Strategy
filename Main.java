971dfb667eb7ff217f582a3d9d313e1646589d7a
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-03-08 07:54:23 +0530

    Update Main.java by adding code for menu options

--- Main.java
+++ Main.java
@@ -8,3 +8,13 @@ import java.util.Scanner;
 public class Main { 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); // Creating a Scanner object for user input
+
+        while (true) {
+            // Display menu options
+            System.out.println("\n--- Duck Simulation ---");
+            System.out.println("1. Mallard Duck");
+            System.out.println("2. Redhead Duck");
+            System.out.println("3. Rubber Duck");
+            System.out.println("4. Decoy Duck");
+            System.out.println("5. Exit");
+            System.out.print("Enter choice: ");
