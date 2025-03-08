9190bd2c30ca016fd8ef7d2eebc60306e8dbe2d7
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-03-08 07:53:09 +0530

    Create Main.java and defining Main class

---
+++ Main.java
@@ -0,0 +1,10 @@
+// Name: Ameya Tipnis
+// PRN: 23070126012
+// Batch: 2023-27 (A1)
+
+import java.util.Scanner;
+
+// Defining main class to run the Duck simulation
+public class Main { 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // Creating a Scanner object for user input
