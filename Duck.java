e9522974f23a8445b99991cd55c9cc63d778305a
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-03-08 08:02:51 +0530

    Create Duck.java and defining an abstract class named Duck

---
+++ Duck.java
@@ -0,0 +1,7 @@
+// Defining an abstract class for common Duck properties
+public abstract class Duck {
+    // Defining protected variables for Duck's name and its behaviors
+    protected String name;
+    protected FlyBehavior flyBehavior;
+    protected QuackBehavior quackBehavior;
+    protected SwimBehavior swimBehavior;
